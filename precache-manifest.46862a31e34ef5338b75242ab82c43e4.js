98c1b8d0d83b964df3e550a45e95827fab10c14f
Mouni Nischal <42888971+Coolchirutha@users.noreply.github.com>
2020-06-12 17:54:14 +0530

    Updates

---
+++ precache-manifest.46862a31e34ef5338b75242ab82c43e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21a0527671d9ff9b4c0de51f107b665",
+    "url": "/record-button/index.html"
+  },
+  {
+    "revision": "bbdca1a298bcc3f987a4",
+    "url": "/record-button/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "79057f5208988f462e58",
+    "url": "/record-button/static/js/2.75ff2655.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/record-button/static/js/2.75ff2655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbdca1a298bcc3f987a4",
+    "url": "/record-button/static/js/main.b5c13728.chunk.js"
+  },
+  {
+    "revision": "20ee40f978d56d32b0d8",
+    "url": "/record-button/static/js/runtime-main.2cd0606b.js"
+  }
+]);
